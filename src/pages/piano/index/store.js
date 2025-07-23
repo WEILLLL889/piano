b0136b894715eda256de78269392a4303e1392a7
Camellia <liujx266@mail2.sysu.edu.cn>
2025-07-23 19:37:36 +0800

    首页背景图片修改

--- src/pages/piano/index/store.js
+++ src/pages/piano/index/store.js
@@ -10,7 +10,7 @@ export default function () {
     state: {
       mainDoc: '', // 主文档
       hideNavBar: false, // 是否隐藏导航条
-      background: localStorage.getItem('background') || 'pianobg.jpg', // 新增：背景图片名称
+      background: localStorage.getItem('background') || 'https://images.unsplash.com/photo-1511379938547-c1f69419868d?auto=format&fit=crop&w=1920', // 新增：背景图片名称
       cacheConf: {
         showKbdNum: true, // 显示键盘上的数字
         scale: 1, // 保存对barScale控制的参数，1-20
